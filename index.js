f48445e4cd08b88875d50ef8ba1f2cbfcfe9d035
Aminesam1092 <doublaliamin13@gmail.com>
2022-06-05 14:21:50 +0900

    セットタイマーイベント制作

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("hello world");
+const FONT = "48px monospace"; //フォントサイズ
+
+//タイマーイベント発生時
+function WmTimer(){
+    const ca = document.getElementById("main"); //メインキャンバスの要素を取得
+    const g =ca.getContext("2d"); //2D 描画コンテキストを取得
+    g.font = FONT;
+    g.fillText("hello world", 0, 64);
+}
+
+// ブラウザ起動イベント
+window.onload = function(){
+
+    setInterval(function(){ WmTimer() }, 60); //60ms間でWmTimer()を呼び出す
+}

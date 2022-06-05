d029e5a348f9c3327749777701edc2ccd7ff57d9
Aminesam1092 <doublaliamin13@gmail.com>
2022-06-05 14:25:13 +0900

    セットタイマー修正

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const FONT = "48px monospace"; //フォントサイズ
 
+let gFrame = 0; ///内部カウンター
 //タイマーイベント発生時
 function WmTimer(){
+    gFrame++; //内部カウンター加算
     const ca = document.getElementById("main"); //メインキャンバスの要素を取得
     const g =ca.getContext("2d"); //2D 描画コンテキストを取得
     g.font = FONT;
-    g.fillText("hello world", 0, 64);
+    g.fillText("hello world" + gFrame, gFrame / 10, 64);
 }
 
 // ブラウザ起動イベント
